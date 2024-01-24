5b0aa212729e8e99b2386166a073a758330df24e
justChillings <tmdgh2840@naver.com>
2024-01-24 15:44:43 +0900

    1/24

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Delete, HttpCode, HttpStatus, Param, ValidationPipe, UsePipes, Patch, ParseUUIDPipe, ParseIntPipe, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateBoardDto } from 'src/DTO/Create_Board.dto';
+import { Board, BoardStatus } from './auth.model';
+import { BoardStatusValidationPipe } from 'src/auth/'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authservice: AuthService) { }
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.authservice.getAllBoards();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(
+    @Param('id',
+      new ParseUUIDPipe
+        ({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+    id: string): Board {
+    return this.authservice.getBoardById(id);
+  }
+  @Patch('/id/status')
+  updateBoardStatus(@Param('id', ParseUUIDPipe) id: string, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
+    return this.authservice.updateBoardStatus(id, status);
+  }
+  @Delete(':/id')
+  @HttpCode(204)
+  deleteboard(@Param('id', ParseUUIDPipe) id : string) : void {
+    this.authservice.deleteboard(id);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Board, BoardStatus } from './auth.model';
+import { CreateBoardDto } from 'src/DTO/Create_Board.dto';
 
-@Injectable()
-export class AuthService {}
+  @Injectable()
+  export class AuthService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+      return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto): Board {
+      const { title, description } = createBoardDto;
+    }
+    const board: Board = {
+      title,
+      description,
+      status: BoardStatus.PUBLIC
+    }
+    this.boards.push(board);
+return board;
+  }
+
+getBoardById(id : string) : Board {
+  const found = this.boards.find(board => board.id === id);
+  if (!found) {
+    throw new NotFoundException('해당 게시글이 존재하지 않습니다. id=${id');
+  }
+  return found;
+}
+
+updateBoardStatus(id : string, status : BoardStatus) : Board {
+  const board = this.getBoardById(id);
+  board.status = status;
+  return board;
+}
+
+deleteBoard(id : string) : void {
+  const found = this.getBoardBYId(id);
+  this.boards = this.boards.filter(board => board.id !== found.id);
+}
